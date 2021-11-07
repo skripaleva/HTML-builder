a4f6497cfbe86d13abc02e3d23c28e915d8f97ce
Evgeniya Skripaleva <evgeniya_ignaten@mail.ru>
2021-11-07 20:34:40 +0300

    feat: resolve task4

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,16 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
-  if(err) return console.error(err);
+  if (err) throw err;
   files.forEach(file => {
     fs.stat(path.join(__dirname, 'secret-folder', file), (err, stats) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
+      if (err) throw err;
       if (stats.isFile()) {
         console.log(path.basename(file, path.extname(file)) + ' - ' + path.extname(file).slice(1) + ' - '
-          + (stats.size/1024).toFixed(3) + 'kb');
+          + stats.size/1024 + 'kb');
       }
     });
   });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderName = `${path.join(__dirname, 'files')}`;
+const folderCopyName = `${path.join(__dirname, 'files-copy')}`;
+
+fs.promises.mkdir(folderCopyName, { recursive: true }, err => {
+  if (err) throw err;
+});
+fs.readdir(folderCopyName, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(path.join(folderCopyName, file), err => {
+      if (err) throw err;
+    });
+  }
+});
+
+function copyDir() {
+  fs.readdir(folderName, (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+      fs.copyFile(path.join(folderName, file), path.join(folderCopyName, file), err => {
+        if (err) throw err;
+      }
+      );
+      console.log(`${file} was copied to folder files-copy`);
+    });
+  });
+}
+
+copyDir();
+
 
